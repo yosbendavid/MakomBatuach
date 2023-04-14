c196662b58f82eceb01964a14176bd585fb9b205
yosbendavid <116652434+yosbendavid@users.noreply.github.com>
2023-04-14 15:41:03 +0300

    patiant, bottom bar, display fix add

---
+++ src/components/Patient/Patient.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import PHomePage from "./PHomePage";
+import "../../CSS/TherapistHomePage.css";
+import BottomBar from "../Template parts/BottomBar"
+
+const Patient = () => {
+    return(
+        <div>
+            
+            <BottomBar />
+        </div>
+    );
+}
+export default Patient;
