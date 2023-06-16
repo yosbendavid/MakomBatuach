af8001e7e75cf30df5178535f0159e6be6421845
yosbendavid <116652434+yosbendavid@users.noreply.github.com>
2023-06-16 15:15:35 +0300

    Yosi therapist schedule (#20)
    * in text box Password Visible update
    
    * New daysoff Schedule page for therapist

--- src/components/Patient/Patient.jsx
+++ src/components/Patient/Patient.jsx
@@ -1,12 +1,10 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../../CSS/Patient.css";
 import PatientNewMeeting from "./Patient New Meeting/PatientNewMeeting";
 import BottomBar from "../Template parts/BottomBar";
 import TopBar from "../Template parts/TopBar";
-import {format} from 'date-fns';
-import { useNavigate, useLocation, } from "react-router-dom";
-
-
+import { format } from "date-fns";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Patient = () => {
   const meetings = [
@@ -36,14 +34,13 @@ const Patient = () => {
     },
   ];
 
-  const navigate=useNavigate();
-  const {state}=useLocation();
-  useEffect(()=>{
-      const email=state;
-      setEmail(email)
-      console.log(email)
-    
-  },[])
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  useEffect(() => {
+    const email = state;
+    setEmail(email);
+    console.log(email);
+  }, []);
 
   const [patientName, setPatientName] = useState("");
   const [therapistName, setTherapistName] = useState("");
@@ -54,124 +51,104 @@ const Patient = () => {
   const [meet, setMeet] = useState("");
   const [email, setEmail] = useState("");
 
+  const patientNameHandle = () => {
+    setPatientName("");
+  };
+  const patientUserClick = () => {};
+  const patientHomeClick = () => {};
+  const patientCalendarClick = () => {};
+  const patientSideBarClick = () => {};
 
+  // הפונקציה שאני מעביר כדי לתפוס את הערך של תאריך, להעביר את הזמנים החדשים ולרנדר
+  const handleMeetingDateChange = (value) => {
+    var t = format(value, "yyyy/MM/dd");
+    console.log(email);
+    var v = format(value, "yyyy/MM/dd hh:mm");
+    setMeetingDate(v);
+    const tryget = "https://localhost:44380/api/amen/";
+    fetch(tryget + t + "/?email=" + email, {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json; charset=UTF-8",
+        Accept: "application/json; charset=UTF-8",
+      }),
+    })
+      .then((res) => {
+        console.log("res=", res);
+        console.log("res.status", res.status);
+        console.log("res.ok", res.ok);
+        return res.json();
+      })
+      .then(
+        (result) => {
+          console.log(result);
+          const indexedHours = result.map((roomNum, index) => {
+            return { id: index, room: roomNum.Room_Num };
+          });
+          //לפה להכניס את הזמנים החדשים
+          setTimeSlots(result);
+          setRoomNum(indexedHours);
+        },
+        //////Not Good!!! "result[2].Room_Num" points to a specific place. Maybe Map functiom?
+        (error) => {
+          console.log("err post=", error);
+        }
+      );
+  };
+  // הפונקציה שאני מעביר כדי לתפוס את הערך של שעות
+  const handleMeetingTimeChange = (value) => {
+    setMeetingTime(value);
+  };
 
-    const patientNameHandle = () => {
-        setPatientName('');
+  const handleRoomNum = (value) => {
+    setRoomNum(roomNum[value].room);
+  };
+  //הפוקנציה שאני מעביר בשביל הכפתור אישור שיקח את המשתנים בזמן הלחיצה
+
+  const Go2Approve = () => {
+    navigate("/meetingApproved");
+  };
+
+  const setNewMeeting = (event) => {
+    event.preventDefault();
+    const newMeeting = {
+      TreatmentDate: meetingDate,
+      WasDone: "n",
+      StartTime: meetingTime,
+      Room_Num: roomNum,
+      Type_Id: 1,
+      Patient_Email: email,
     };
-    const patientUserClick = () => {
-        
-    };
-    const patientHomeClick = () => {
-        
-    };
-    const patientCalendarClick = () => {
-        
-    };
-    const patientSideBarClick = () => {
-        
-    };
-
-      
-    // הפונקציה שאני מעביר כדי לתפוס את הערך של תאריך, להעביר את הזמנים החדשים ולרנדר
-    const handleMeetingDateChange = (value) => {
-        var t= format(value, 'yyyy/MM/dd');
-        console.log(email);
-        var v= format(value, 'yyyy/MM/dd hh:mm');
-        setMeetingDate(v);
-       const tryget='https://localhost:44380/api/amen/'
-       fetch(tryget+t+'/?email='+email,
-       {
-           method: 'GET',
-           headers: new Headers({
-           'Content-Type': 'application/json; charset=UTF-8',
-           'Accept': 'application/json; charset=UTF-8',
-           })
-           })
-           .then(res => {
-           console.log('res=', res);
-           console.log('res.status', res.status);
-           console.log('res.ok', res.ok);
-           return res.json()
-           })
-           .then(
-               (result) => {
-                console.log(result);
-                const indexedHours = result.map((roomNum, index) => {
-                    return {id: index, room:roomNum.Room_Num};
-                  });      
-                          //לפה להכניס את הזמנים החדשים          
-                  setTimeSlots(result);
-                  setRoomNum(indexedHours);
-               },
-               //////Not Good!!! "result[2].Room_Num" points to a specific place. Maybe Map functiom?
-           (error) => {
-           console.log("err post=", error);
-           });    
-
-
-    }
-    // הפונקציה שאני מעביר כדי לתפוס את הערך של שעות
-    const handleMeetingTimeChange = (value) => {
-        setMeetingTime(value);
-    }
-
-    const handleRoomNum = (value) => {
-        setRoomNum(roomNum[value].room);
-    }
-    //הפוקנציה שאני מעביר בשביל הכפתור אישור שיקח את המשתנים בזמן הלחיצה
-        
-    const Go2Approve = () => {
-        navigate("/meetingApproved");
-      }
-
-    const setNewMeeting = (event) => 
-    {
-        event.preventDefault();
-        const newMeeting = 
-        {      
-                TreatmentDate : meetingDate,
-                WasDone : "n",
-                StartTime : meetingTime,
-                Room_Num :roomNum,
-                Type_Id: 1,
-                Patient_Email:email,
-          }
-        console.log(newMeeting);
+    console.log(newMeeting);
 
     const apiUrl = "https://localhost:44380/api/createtre";
 
-        try {
-
-        fetch(apiUrl, 
-            {
-            method: 'POST',
-            body: JSON.stringify(newMeeting),
-            headers: new Headers({
-            'Content-type': 'application/json; charset=UTF-8' 
-            })
-            })
-            .then(res => {
-            console.log('res=', res);
-            if(res.status === 200)
-            Go2Approve();
-            {
-                
-            }
-            })
-            .then(
-            (result) => {
+    try {
+      fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(newMeeting),
+        headers: new Headers({
+          "Content-type": "application/json; charset=UTF-8",
+        }),
+      })
+        .then((res) => {
+          console.log("res=", res);
+          if (res.status === 200) Go2Approve();
+          {
+          }
+        })
+        .then(
+          (result) => {
             console.log("fetch POST= ", result);
-            },
-            (error) => {
+          },
+          (error) => {
             console.log("err post=", error);
-            });
-
-        }
-        catch (error) {
-            console.error('Request failed with status code', error.response.status);
-        }
+          }
+        );
+    } catch (error) {
+      console.error("Request failed with status code", error.response.status);
     }
+  };
 
   return (
     <div className="patient-container-div">
